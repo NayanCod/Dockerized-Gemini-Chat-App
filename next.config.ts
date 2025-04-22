791b92777be4c3ae2a46c5416bc608d8657807c3
NayanCod <kumarnayab6@gmail.com>
2025-04-22 11:45:23 +0530

    dockerize the app

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_GEMINI_KEY: process.env.NEXT_PUBLIC_GEMINI_KEY,
+  }
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Dockerized",
+  title: "gemini-chat",
   description: "containerized withh docker",
 };
 
